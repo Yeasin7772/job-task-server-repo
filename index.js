3198bc6dea99a2a1879198ced3d5be7988437338
Yeasin7772 <yeasin26yeasin26@gmail.com>
2023-12-22 19:53:44 +0600

    deploy vercle

--- index.js
+++ index.js
@@ -24,9 +24,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const jobTaskCollection = client.db("jobtask").collection("job");
+    const benefitCollection = client.db("jobtask").collection("benefit");
 
 
 
@@ -44,6 +45,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/benefit", async (req, res) => {
+      const result = await benefitCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobTaskCollection.findOne(query);
+      res.send(result);
+    });
+    
+
     // deleted method 
 
     app.delete("/job/:id", async (req, res) => {
@@ -52,6 +66,44 @@ async function run() {
       const result = await jobTaskCollection.deleteOne(query);
       res.send(result);
     });
+
+
+
+    app.patch("/job/:id", async (req, res) => {
+      const taskData = req.body;
+      console.log(taskData);
+
+      const taskId = req.params.id;
+      
+
+      const query = { _id: new ObjectId(taskId) };
+      console.log(query);
+
+      const editTask = {
+          $set: {
+              title: taskData.title,
+              category: taskData.category,
+              date: taskData.date,
+              description: taskData.description,
+              userEmail: taskData.userEmail,
+          },
+      };
+
+      try {
+          const result = await jobTaskCollection.updateOne(query, editTask);
+
+          if (result.modifiedCount > 0) {
+              res.json({ modifiedCount: result.modifiedCount });
+          } else {
+              res.status(404).json({ message: 'Task not found or no changes applied' });
+          }
+      } catch (error) {
+          console.error('Error updating task:', error);
+          res.status(500).json({ message: 'Internal server error' });
+      }
+  });
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
